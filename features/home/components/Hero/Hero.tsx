2da53afab8cf998cd97066fdeb16b55905200020
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2026-02-23 00:30:53 +0700

    fix(hero): add Suspense boundary for WebGL components

--- features/home/components/Hero/Hero.tsx
+++ features/home/components/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Environment } from "@react-three/drei";
 import {
@@ -52,43 +53,45 @@ export default function Hero() {
           camera={{ position: [0, 0, 8], fov: 35 }}
           gl={{ antialias: false }}
         >
-          <InteractiveGothicBackground />
+          <Suspense fallback={null}>
+            <InteractiveGothicBackground />
 
-          <fog attach="fog" args={["#050505", 3, 15]} />
+            <fog attach="fog" args={["#050505", 3, 15]} />
 
-          <ambientLight intensity={0.05} />
-          <spotLight
-            position={[3, 8, 4]}
-            angle={0.5}
-            penumbra={1}
-            intensity={2.5}
-            castShadow
-            color="#ffffff"
-          />
-          <pointLight
-            position={[-2, -5, -2]}
-            intensity={50}
-            color="#8a0303"
-            distance={10}
-            decay={2}
-          />
-
-          <GothicComponentScene />
-
-          <Environment
-            files="/hdr/qwantani_night_puresky_2k.hdr"
-            environmentIntensity={0.05}
-          />
-
-          <EffectComposer multisampling={4}>
-            <Noise
-              opacity={0.6}
-              premultiply
-              blendFunction={BlendFunction.OVERLAY}
+            <ambientLight intensity={0.05} />
+            <spotLight
+              position={[3, 8, 4]}
+              angle={0.5}
+              penumbra={1}
+              intensity={2.5}
+              castShadow
+              color="#ffffff"
             />
-            <Vignette eskil={false} offset={0.15} darkness={1.2} />
-            <ChromaticAberration offset={new THREE.Vector2(0.0015, 0.0015)} />
-          </EffectComposer>
+            <pointLight
+              position={[-2, -5, -2]}
+              intensity={50}
+              color="#8a0303"
+              distance={10}
+              decay={2}
+            />
+
+            <GothicComponentScene />
+
+            <Environment
+              files="/hdr/qwantani_night_puresky_2k.hdr"
+              environmentIntensity={0.05}
+            />
+
+            <EffectComposer multisampling={4}>
+              <Noise
+                opacity={0.6}
+                premultiply
+                blendFunction={BlendFunction.OVERLAY}
+              />
+              <Vignette eskil={false} offset={0.15} darkness={1.2} />
+              <ChromaticAberration offset={new THREE.Vector2(0.0015, 0.0015)} />
+            </EffectComposer>
+          </Suspense>
         </Canvas>
       </div>
 
